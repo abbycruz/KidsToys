65cbffa57e82bc17f622db8d802fd9d5b6c9b011
abby <Los Crack@ALEJANDRA>
2022-02-10 15:44:37 -0600

    primera parte del proyecto

---
+++ KidsToys/KidsToys.BL/ProductosBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsToys.BL
+{
+    public class ProductosBL
+    {
+        Contexto _contexto;
+        public List<Producto>ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();    
+        }
+
+       public List<Producto> ObtenerProductos()
+        {
+
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
+          
+        }    
+      
+    }
+}
