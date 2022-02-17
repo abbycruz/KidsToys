9bc050b35c71c7b9c63e06a6b5d7c506a6cdef8f
abby <Los Crack@ALEJANDRA>
2022-02-17 12:05:20 -0600

    Avance del proyecto segunda parte

--- KidsToys/KidsToys.BL/Categoria.cs
+++ KidsToys/KidsToys.BL/Categoria.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KidsToys.BL
 {
-   public class Producto
+    public class Categoria
     {
-
-        public Producto()
-        {
-            Activo = true;
-        }
         public int Id { get; set; }
         public string Descripcion { get; set; }
-        public double Precio { get; set; }
-        public Categoria Categoria { get; set; }
-        public bool Activo { get; set; }
 
     }
 }
--- KidsToys/KidsToys.BL/Contexto.cs
+++ KidsToys/KidsToys.BL/Contexto.cs
@@ -22,5 +22,6 @@ namespace KidsToys.BL
         }
 
         public DbSet<Producto>Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
     }
 }
